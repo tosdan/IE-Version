901c8d6063046f6760e730ef1b873f1aa88d4c22
tosdan <stdanielets@gmail.com>
2015-04-16 20:59:12 +0200

    jQuery dependency for deferred

--- IE-Version.js
+++ IE-Version.js
@@ -1,32 +1,151 @@
-window.IE = {
-			DEBUG: false
+window.IE = (function () {
+	var DEBUG = false,
+		dfd = $.Deferred(),
+		IEOBJ = {};
+
+
+	IEOBJ.ieBrowserVersion = (function() {
+		var retval = 0,
+			dfd1 = $.Deferred();
+
+		try {
+			 // Non supportato da IE 11
+			retval = /*@cc_on (function() { return jscriptEngineVer(@_jscript_version); }()) || 0 ;@*/ 0;		
+			if (console && DEBUG) {
+				console.log("@_jscript_version: " + retval);
+			}
+		} catch (e) {
+			retval = 0;
+			if (console && DEBUG) {
+				console.log("Error @_jscript_version: " + e.message);
+			}
 		}
 
-IE.ieBrowserVersion = (function() {
-	var retval = 0
-
-	try {
-		 // Non supportato da IE 11
-		retval = /*@cc_on (function() { return jscriptEngineVer(@_jscript_version); }()) || 0 ;@*/ 0;		
-		if (console && IE.DEBUG) {
-			console.log("@_jscript_version: " + retval);
+		if (!retval && !!window.ScriptEngineMajorVersion) {
+			// supportato anche da IE 11
+			retval = ScriptEngineMajorVersion() + '.' + ScriptEngineMinorVersion();
+			retval = jscriptEngineVer(retval);
+			if (console && DEBUG) {
+				console.log("ScriptEngine Major+.+Minor Version: " + retval);	
+			}
 		}
-	} catch (e) {
-		retval = 0;
-		if (console && IE.DEBUG) {
-			console.log("Error @_jscript_version: " + e.message);
-		}
-	}
-
-	if (!retval && !!window.ScriptEngineMajorVersion) {
-		// supportato anche da IE 11
-		retval = ScriptEngineMajorVersion() + '.' + ScriptEngineMinorVersion();
-		retval = jscriptEngineVer(retval);
-		if (console && IE.DEBUG) {
-			console.log("ScriptEngine Major+.+Minor Version: " + retval);	
-		}
-	}
+		
+		dfd1.resolve(retval);
+		
+		/**** Questi controlli vengono falsati dal documentMode
+		if (!retval) {
+			if (Object.hasOwnProperty.call(window, "ActiveXObject") && !window.ActiveXObject) {
+				retval = 11;
+			} else {
+				retval = 10;	
+			}
+			if (!window.atob) 				retval = 9;
+			if (!document.addEventListener) retval = 8;
+			if (!document.querySelector) 	retval = 7;
+			if (!window.XMLHttpRequest) 	retval = 6;
+			if (!document.compatMode) 		retval = 5;	
+		} 
+		******/
+		
+		return dfd1.promise();
+	}());
 	
+	IEOBJ.ieDocumentMode = (function() {
+		var dfd3 = $.Deferred();
+		dfd3.resolve(document.documentMode);
+		return dfd3.promise();
+	}());
+
+	IEOBJ.ieBrowserMode = (function() {
+		var retval,
+			dfd2 = $.Deferred();
+		// ----------------------------------------------------------
+		// A short snippet for detecting versions of IE in JavaScript
+		// without resorting to user-agent sniffing
+		// https://gist.github.com/padolsey/527683
+		// ----------------------------------------------------------
+		// If you're not in IE (or IE version is less than 5) then:
+		//		     ie === undefined
+		// If you're in IE (>=5) then you can determine which version:
+		//		     ie === 7; // IE7
+		// Thus, to detect IE:
+		//		     if (ie) {}
+		// And to detect the version:
+		//		     ie === 6 // IE6
+		//		     ie > 7 // IE8, IE9 ...
+		//		     ie < 9 // Anything less than IE9
+		// ----------------------------------------------------------
+			 
+		// UPDATE: Now using Live NodeList idea from @jdalton
+		 
+	    var undef,
+	        v = 3,
+	        div = document.createElement('div'),
+	        all = div.getElementsByTagName('i');
+
+		// the while loop is used without an associated block: {}
+		// so, only the condition within the () is executed.
+
+		// semicolons arent allowed within the condition,
+		//   so a comma is used to stand in for one
+		// basically allowing the two separate statements 
+		//   to be evaluated sequentially.
+			
+	    while (
+	        div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+	        all[0]
+	    );
+		
+		// each time it's evaluated, v gets incremented and
+		//   tossed into the DOM as a conditional comment
+		// the i element is then a child of the div.
+
+		// the return value of the getEBTN call is used as 
+		//   the final condition expression
+		// if there is an i element (the IE conditional
+		//   succeeded), then getEBTN's return is truthy
+		// and the loop continues until there is no 
+		//   more i elements.
+
+		// In other words:  ** MAGIC**
+
+	    retval = v > 4 ? v : undef;
+
+	    // Per determinare il Browser Mode di IE >= 10 che non supporta piu' i conditional comments
+		if (IEOBJ.ieBrowserVersion && !retval) {
+			var msieLT11 = (/MSIE[\s]*([\d\.]+)/),
+				msieGTE11 = (/rv:([\d\.]+)/),
+				appVer = navigator.appVersion,
+				
+				result = [];
+			try {
+				if (msieLT11.test(appVer)) {
+					result = msieLT11.exec(appVer);
+					
+				} else if (msieGTE11.test(appVer)) {
+					result = msieGTE11.exec(appVer);
+					
+				} else {
+					result[1] = "<unknown>";
+				}
+				
+			} catch (e) {
+				result[1] = "<error>";
+				if (console && DEBUG) {
+					console.log(e.message);
+				}
+			}
+			
+			retval = result[1];
+			
+		}
+		
+		dfd2.resolve(retval);
+		
+		return dfd2.promise();
+	}());
+
+
 	function jscriptEngineVer(engineVer) {
 		var v = Number(engineVer);
 	    switch (v) {
@@ -44,105 +163,15 @@ IE.ieBrowserVersion = (function() {
 	        case 12:  return 12;
 	    }
 	}
-	/**** Questi controlli vengono falsati dal documentMode
-	if (!retval) {
-		if (Object.hasOwnProperty.call(window, "ActiveXObject") && !window.ActiveXObject) {
-			retval = 11;
-		} else {
-			retval = 10;	
-		}
-		if (!window.atob) 				retval = 9;
-		if (!document.addEventListener) retval = 8;
-		if (!document.querySelector) 	retval = 7;
-		if (!window.XMLHttpRequest) 	retval = 6;
-		if (!document.compatMode) 		retval = 5;	
-	} 
-	******/
 	
-	return retval;
+	$.when(IEOBJ.ieBrowserVersion, IEOBJ.ieDocumentMode, IEOBJ.ieBrowserMode).then(function(ieBrowserVersion, ieDocumentMode, ieBrowserMode) {
+		var result = {
+				ieBrowserVersion: ieBrowserVersion,
+				ieDocumentMode: ieDocumentMode, 
+				ieBrowserMode: ieBrowserMode
+		};
+		dfd.resolve(result);
+	});
+	
+	return dfd.promise();
 }());
-
-IE.ieDocumentMode = document.documentMode;
-
-IE.ieBrowserMode = (function() {
-// ----------------------------------------------------------
-// A short snippet for detecting versions of IE in JavaScript
-// without resorting to user-agent sniffing
-// https://gist.github.com/padolsey/527683
-// ----------------------------------------------------------
-// If you're not in IE (or IE version is less than 5) then:
-//	     ie === undefined
-// If you're in IE (>=5) then you can determine which version:
-//	     ie === 7; // IE7
-// Thus, to detect IE:
-//	     if (ie) {}
-// And to detect the version:
-//	     ie === 6 // IE6
-//	     ie > 7 // IE8, IE9 ...
-//	     ie < 9 // Anything less than IE9
-// ----------------------------------------------------------
- 
-// UPDATE: Now using Live NodeList idea from @jdalton
-	 
-    var undef,
-        v = 3,
-        div = document.createElement('div'),
-        all = div.getElementsByTagName('i');
-
-	// the while loop is used without an associated block: {}
-	// so, only the condition within the () is executed.
-
-	// semicolons arent allowed within the condition,
-	//   so a comma is used to stand in for one
-	// basically allowing the two separate statements 
-	//   to be evaluated sequentially.
-		
-    while (
-        div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
-        all[0]
-    );
-	
-	// each time it's evaluated, v gets incremented and
-	//   tossed into the DOM as a conditional comment
-	// the i element is then a child of the div.
-
-	// the return value of the getEBTN call is used as 
-	//   the final condition expression
-	// if there is an i element (the IE conditional
-	//   succeeded), then getEBTN's return is truthy
-	// and the loop continues until there is no 
-	//   more i elements.
-
-	// In other words:  ** MAGIC**
-
-    return v > 4 ? v : undef;
-    
-}());
-
-if (IE.ieBrowserVersion && !IE.ieBrowserMode) {
-	var msieLT11 = (/MSIE[\s]*([\d\.]+)/),
-		msieGTE11 = (/rv:([\d\.]+)/),
-		appVer = navigator.appVersion,
-		
-		result = [];
-	try {
-		if (msieLT11.test(appVer)) {
-			result = msieLT11.exec(appVer);
-			
-		} else if (msieGTE11.test(appVer)) {
-			result = msieGTE11.exec(appVer);
-			
-		} else {
-			result[1] = "<unknown>";
-		}
-		
-	} catch (e) {
-		result[1] = "<error>";
-		if (console && IE.DEBUG) {
-			console.log(e.message);
-		}
-	}
-	
-	IE.ieBrowserMode = result[1];
-		
-}
